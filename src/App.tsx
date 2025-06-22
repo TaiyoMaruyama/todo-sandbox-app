d8cdce35dc787b13ac80523c7299542e32d0003c
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2025-06-22 09:26:55 +0900

    react-router-domを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <div>demo</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
